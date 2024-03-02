28d1fc53431da9c4e10b309955a994778d8dd567
tarek blkraiet <tarekblkraiet@gmail,com>
2024-03-02 11:19:31 +0100

    modify code en register.html and js and register html and js #67

--- staff/tarek-blkraiet/liveshare/login.js
+++ staff/tarek-blkraiet/liveshare/login.js
@@ -6,19 +6,20 @@ form.onsubmit = function (event) {
     event.preventDefault()
 
       var usernameInput = form.querySelector('#username')
-      var username =usernameInput.value
+      var username = usernameInput.value
 
-      var passwordInput =form.querySelector('#password')
-      var password=passwordInput.value
+      var passwordInput = form.querySelector('#password')
+      var password= passwordInput.value
 
       try{
-        loginUser(username,password)
+        loginUser(username, password)
 
         console.log('user logged in')
 
         alert('user logged in')
 
-        form.reset() // para clean los especios despues de escribir
+        form.reset() 
+        // para clean los especios despues de escribir
 
         //TODO navigate to home
       } catch (error) {
