6361488c11bca15da9204b49656209de3c782311
tarek blkraiet <tarekblkraiet@gmail,com>
2024-03-12 09:40:12 +0100

    resolve error #158

--- staff/tarek-blkraiet/strings/slice.js
+++ staff/tarek-blkraiet/strings/slice.js
@@ -2,6 +2,16 @@
 function slice(string,indexStart, indexEnd){
     var result=''
 
+    // if (indexStart === -indexStart)
+    // if (indexFind === -indexEnd)
+
+    if (index < 0){
+        indexStart = string.length + indexStart;
+    }
+    if (index < 0) {
+        index = string.length +indexEnd;
+    }
+
     for(var i= indexStart;i<indexEnd;i++){
         result = result + string[i] // para ir acumulando
 
@@ -12,7 +22,9 @@ function slice(string,indexStart, indexEnd){
 
 console.log('case1 print caracters from 6 to 10')
 var string = 'Estoy aprendiendo programacion'
-console.log(string.slice(6,17));
+var result = slice(string,6,11)
+console.log({ expected:'apren',received:result});
+
 
 
 console.log('case 2 print characters from from -30 to-12 ')
