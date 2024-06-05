11aa78661ea893165f7e5ac02969ec28aea9c88d
tarek blkraiet <tarekblkraiet@gmail,com>
2024-06-05 11:09:52 +0200

    modifly data #175

--- staff/tarek-blkraiet/arrays/map.js
+++ staff/tarek-blkraiet/arrays/map.js
@@ -1,63 +1,70 @@
-/*/usando el map, crea un nuevo Array, con cada uno de los numberos multiplicando por 2
+//usando el map, crea un nuevo Array, con cada uno de los numberos multiplicando por 2
 
-var numbers = [ 10,20, 30, 40, 50, 60]
-
-var numbersx2 = numbers.map( function(x){
-    return x* 2
+var nums = [10, 20, 30, 40, 50, 60]
+console.log('CASE1 : return an array of numbers multipled by 2')
+var case1 = nums.map(function (nums) {
+    return nums * 2
 })
-console.log(numbersx2)
- //-------------------------------
+console.log(case1)
 
- //crea una nueva array, con la longitud de cada de los 
+var strings = ['hola', 'mundo', 'a', 'todos']
+console.log('CASE 2 : return an array with the length of all the strings')
+var case2 = strings.map(function (strings) {
+    return strings.length
+})
+console.log(case2)
 
- var strings = ['hola', 'mundo', 'a',' todos']
+var numbers2 = [12, 40, 6, 7, 24]
+console.log('CASE3 : return an array with the remainder of each number divided by 2')
+var case3 = numbers2.map(function (number) {
+    return number % 2
+})
+console.log(case3)
 
- //var 
+var users = [
+    { name: 'pepito', email: 'pepito@grillo.com' },
+    { name: 'wendy', email: 'wendy@darling.com' },
+    { name: 'peter', email: 'peter@pan.com' },
+    { name: 'pinocho', email: 'pin@ocho.com' }
+]
+console.log('CASE 4: return an array that returns each email from users')
 
- //----------------------------------
- //crea una nuevo Array,
- // que contenga los emails de los usuarios como string
+var case4 = users.map(function (user) {
+    return user.email
+})
+console.log(case4)
+// crea un nuevo array,
+// que contenga cuidad de los usuarios como string
 
- var users =[
-    { name: 'pepito', email:'pepito@grillo.com'},
-    { name:'wendy', email:'wendy@darling.com'},
-    {name:'peter', email:'peter@pan.com'},
-    {name:'pinocho', email:'pin@ocho.com'}
- ]
- var email = users.map(function(user){
-  return user.email
- })*/
- // crea un nuevo array,
- // que contenga cuidad de los usuarios como string
-
- var users2 = [
+var users2 = [
     {
-        name: 'pepito', information:{
-            city:'Madrid',
-            number:'65787959'
+        name: 'pepito', information: {
+            city: 'Madrid',
+            number: '65787959'
         }
     },
     {
-        name: 'wendy', information:{
-            city:'Barcelona',
-            number:'7869406'
+        name: 'wendy', information: {
+            city: 'Barcelona',
+            number: '7869406'
         }
     },
     {
-        name:'peter',information :{
-            city:'sevilla',
-            number:'78795040'
+        name: 'peter', information: {
+            city: 'sevilla',
+            number: '78795040'
         }
     },
     {
-        name:'pinocho', information:{
-            city:'valencia',
-            number:'57694068'
+        name: 'pinocho', information: {
+            city: 'valencia',
+            number: '57694068'
         }
     }
- ]
- var cities = users2.map(function(user){
+]
+console.log('CASE 5: return an array with the city of each user')
+var case5 = users2.map(function (user) {
     return user.information.city
 
- })
- console.log(cities)
+})
+console.log(case5)
