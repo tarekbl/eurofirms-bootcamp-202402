53a0e80c6000e7a97b449c645345bcb3419baa1c
tarek blkraiet <tarekblkraiet@gmail,com>
2024-06-24 18:35:23 +0200

    correct error #263

--- staff/tarek-blkraiet/carray/carray.js
+++ staff/tarek-blkraiet/carray/carray.js
@@ -1,96 +1,98 @@
-function Carray() {
-    var elements = arguments
+class Carray {
+    constructor() {
+        var elements = arguments
 
-    for (var i = 0; i < elements.length; i++) {
-        var element = element[i]
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i]
 
-        this[i] = element
+            this[i] = element
+        }
+        this.length = elements.length
     }
-    this.length = elements.length
-}
-Carray.prototype.forEach = function (callback) {
-    for (var i = 0; i < this.length; i++) {
-        var element = this[i]
+    forEach(callback) {
+        for (var i = 0; i < this.length; i++) {
+            var element = this[i]
 
-        callback(element)
+            callback(element)
+        }
+    }
+    find(callback) {
+        for (var i = 0; i < this.length; i++) {
+            var element = this[i]
+
+            var matches = callback(element)
+
+            if (matches)
+                return element
+
+        }
+    }
+    map(callback) {
+        var results = new carray
+
+        for (var i = 0; i < this.length; i++) {
+            var element = this[i]
+
+            var mappedElement = callback(element)
+
+            results[results.length] = mappedElement
+            results.length++
+        }
+        return results
+    }
+    push() {
+        var elements = arguments
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i]
+
+            this[this.length] = element
+            this.length++
+        }
+        return this.length
+    }
+    pop() {
+        var result = this[this.length - 1]
+        delete this[this.length - 1]
+
+        this.length--
+        return result
+    }
+    shift() {
+        var deletedElement = this[0]
+
+        for (var i = 0; i < this.length - 1; i++) {
+
+            this[i] = this[i + 1]
+        }
+
+        this.length--
+
+        delete this[this.length]
+
+        return deletedElement
+    }
+    unshift() {
+        var elements = arguments
+
+        for (var i = this.length - 1; i >= 0; i++) {
+            this[i + elements.length] = this[i]
+
+        }
+        for (var i = 0; i < elements.length; i++) {
+            this[i] = elements[i]
+            this.length++
+        }
+        return this.length
+    }
+    every(callback) {
+        for (var i = 0; i < this.length; i++) {
+            if (!callback(this[i]))
+                return false
+        }
+        return true
     }
 }
 
-Carray.prototype.find = function (callback) {
-    for (var i = 0; i < this.length; i++) {
-        var element = this[i]
 
-        var matches = callback(element)
 
-        if (matches)
-            return element
-
-    }
-}
-
-Carray.prototype.map = function (callback) {
-    var results = new carray
-
-    for (var i = 0; i < this.length; i++) {
-        var element = this[i]
-
-        var mappedElement = callback(element)
-
-        results[results.length] = mappedElement
-        results.length++
-    }
-    return results
-}
-Carray.prototype.push = function () {
-    var elements = arguments
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i]
-
-        this[this.length] = element
-        this.length++
-    }
-    return this.length
-}
-Carray.prototype.pop = function () {
-    var result = this[this.length - 1]
-    delete this[this.length - 1]
-
-    this.length--
-    return result
-}
-
-Carray.prototype.shift = function () {
-    var deletedElement = this[0]
-
-    for (var i = 0; i < this.length - 1; i++) {
-
-        this[i] = this[i + 1]
-    }
-
-    this.length--
-
-    delete this[this.length]
-
-    return deletedElement
-}
-Carray.prototype.unshift = function () {
-    var elements = arguments
-
-    for (var i = this.length - 1; i >= 0; i++) {
-        this[i + elements.length] = this[i]
-
-    }
-    for (var i = 0; i < elements.length; i++) {
-        this[i] = elements[i]
-        this.length++
-    }
-    return this.length
-}
-Carray.prototype.every = function (callback) {
-    for (var i = 0; i < this.length; i++) {
-        if (!callback(this[i]))
-            return false
-    }
-    return true
-}
-module.exports = Carray
+module.export =Carray
